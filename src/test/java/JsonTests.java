dbc5761071ad743090321aae5fb465f639cd2575
Nick Ng <ngyewch@gmail.com>
2021-09-16 15:11:41 +0800

    updated JSON tests

--- src/test/java/JsonTests.java
+++ src/test/java/JsonTests.java
@@ -3,14 +3,18 @@ import org.arl.fjage.param.ParameterReq;
 import org.arl.fjage.param.ParameterRsp;
 import org.arl.fjage.remote.JsonMessage;
 import org.arl.fjage.sentuator.ConfigParam;
+import org.arl.fjage.sentuator.GenericMeasurement;
+import org.arl.fjage.sentuator.Quantity;
 import org.arl.fjage.sentuator.SentuatorParam;
 import org.junit.Test;
+
+import java.util.UUID;
 
 public class JsonTests
     extends AbstractTests {
 
   @Test
-  public void testConfigParamWithoutConfig() {
+  public void testParameterRspWithoutCustomConfig() {
     final ParameterReq parameterReq = new ParameterReq();
     final ParameterRsp parameterRsp = new ParameterRsp(parameterReq);
     parameterRsp.set(SentuatorParam.enable, true, false);
@@ -18,12 +22,25 @@ public class JsonTests
   }
 
   @Test
-  public void testConfigParamWithCustomConfig() {
+  public void testParameterRspWithCustomConfig() {
     final ParameterReq parameterReq = new ParameterReq();
     final ParameterRsp parameterRsp = new ParameterRsp(parameterReq);
     parameterRsp.set(SentuatorParam.enable, true, false);
     parameterRsp.set(new ConfigParam("level"), 1, false);
     serializeAndDeserialize(parameterRsp);
+  }
+
+  @Test
+  public void testGenericMeasurement() {
+    final GenericMeasurement m = new GenericMeasurement();
+    m.setMessageID(UUID.randomUUID().toString());
+    m.setInReplyTo(UUID.randomUUID().toString());
+    m.setSentAt(System.currentTimeMillis());
+    m.setSensorType("mySensorType");
+    m.set("salinity", new Quantity(35 + Math.random(), "ppt"));
+    m.set("temperature", new Quantity(27 + Math.random(), "C"));
+    m.set("depth", new Quantity(1 + Math.random(), "m"));
+    serializeAndDeserialize(m);
   }
 
   private void serializeAndDeserialize(Message message) {
